c109304d2000093012d643e791fd38ac3364e26b
Scott W Harden <swharden@gmail.com>
2024-04-03 14:17:47 -0400

    place data and graph in split container

--- src/RatioIJ2OL/Form1.Designer.cs
+++ src/RatioIJ2OL/Form1.Designer.cs
@@ -38,11 +38,16 @@ partial class Form1
         nudBaseline1 = new NumericUpDown();
         label1 = new Label();
         nudBaseline2 = new NumericUpDown();
+        splitContainer1 = new SplitContainer();
         ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
         ((System.ComponentModel.ISupportInitialize)nudSelectedRoi).BeginInit();
         ((System.ComponentModel.ISupportInitialize)nudSweeps).BeginInit();
         ((System.ComponentModel.ISupportInitialize)nudBaseline1).BeginInit();
         ((System.ComponentModel.ISupportInitialize)nudBaseline2).BeginInit();
+        ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+        splitContainer1.Panel1.SuspendLayout();
+        splitContainer1.Panel2.SuspendLayout();
+        splitContainer1.SuspendLayout();
         SuspendLayout();
         // 
         // btnSelectCsv
@@ -57,13 +62,13 @@ partial class Form1
         // 
         // dataGridView2
         // 
-        dataGridView2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
         dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-        dataGridView2.Location = new Point(11, 51);
+        dataGridView2.Dock = DockStyle.Fill;
+        dataGridView2.Location = new Point(0, 0);
         dataGridView2.Margin = new Padding(2);
         dataGridView2.Name = "dataGridView2";
         dataGridView2.RowHeadersWidth = 62;
-        dataGridView2.Size = new Size(603, 486);
+        dataGridView2.Size = new Size(500, 486);
         dataGridView2.TabIndex = 2;
         // 
         // label2
@@ -87,12 +92,12 @@ partial class Form1
         // 
         // formsPlot1
         // 
-        formsPlot1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
         formsPlot1.DisplayScale = 1.5F;
-        formsPlot1.Location = new Point(619, 41);
+        formsPlot1.Dock = DockStyle.Fill;
+        formsPlot1.Location = new Point(0, 0);
         formsPlot1.Margin = new Padding(2);
         formsPlot1.Name = "formsPlot1";
-        formsPlot1.Size = new Size(880, 500);
+        formsPlot1.Size = new Size(736, 486);
         formsPlot1.TabIndex = 7;
         // 
         // nudSweeps
@@ -146,20 +151,36 @@ partial class Form1
         nudBaseline2.TabIndex = 12;
         nudBaseline2.Value = new decimal(new int[] { 75, 0, 0, 131072 });
         // 
+        // splitContainer1
+        // 
+        splitContainer1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+        splitContainer1.Location = new Point(8, 45);
+        splitContainer1.Name = "splitContainer1";
+        // 
+        // splitContainer1.Panel1
+        // 
+        splitContainer1.Panel1.Controls.Add(dataGridView2);
+        // 
+        // splitContainer1.Panel2
+        // 
+        splitContainer1.Panel2.Controls.Add(formsPlot1);
+        splitContainer1.Size = new Size(1240, 486);
+        splitContainer1.SplitterDistance = 500;
+        splitContainer1.TabIndex = 13;
+        // 
         // Form1
         // 
         AutoScaleDimensions = new SizeF(7F, 15F);
         AutoScaleMode = AutoScaleMode.Font;
-        ClientSize = new Size(1508, 548);
+        ClientSize = new Size(1260, 543);
+        Controls.Add(splitContainer1);
         Controls.Add(nudBaseline2);
         Controls.Add(nudBaseline1);
         Controls.Add(label1);
         Controls.Add(label3);
         Controls.Add(nudSweeps);
-        Controls.Add(formsPlot1);
         Controls.Add(nudSelectedRoi);
         Controls.Add(label2);
-        Controls.Add(dataGridView2);
         Controls.Add(btnSelectCsv);
         Margin = new Padding(2);
         Name = "Form1";
@@ -170,6 +191,10 @@ partial class Form1
         ((System.ComponentModel.ISupportInitialize)nudSweeps).EndInit();
         ((System.ComponentModel.ISupportInitialize)nudBaseline1).EndInit();
         ((System.ComponentModel.ISupportInitialize)nudBaseline2).EndInit();
+        splitContainer1.Panel1.ResumeLayout(false);
+        splitContainer1.Panel2.ResumeLayout(false);
+        ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+        splitContainer1.ResumeLayout(false);
         ResumeLayout(false);
         PerformLayout();
     }
@@ -186,4 +211,5 @@ partial class Form1
     private NumericUpDown nudBaseline1;
     private Label label1;
     private NumericUpDown nudBaseline2;
+    private SplitContainer splitContainer1;
 }
